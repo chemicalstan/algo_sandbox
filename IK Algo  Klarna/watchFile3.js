8a952f3585cb410d1dd83b2e2bda79229a7d2857
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-06-22 23:29:49 +0100

    Progress

--- IK Algo  Klarna/watchFile3.js
+++ IK Algo  Klarna/watchFile3.js
@@ -1,25 +1,31 @@
 function findRoutes(routes) {
-  //   if (routes.length === 1 && (typeof routes)==='object') {
-  //     return routes.join(" ")
-  //   }
+  // If input is string
   if (typeof routes === "string") {
     let strArr = routes.split(", ");
     // if (strArr.length > 1) {
-      let strSet = new Set(strArr);
-      strArr = [...strSet];
-      return strArr.join(", ");
+    let strSet = new Set(strArr);
+    strArr = [...strSet];
+    return strArr.join(", ");
     // } else {
     //   strArr = strArr.join(" ");
     //   strArr = strArr.split(" ")
     //   return strArr.join(", ");
     // }
   }
-  let flat = routes.reduce((acc, val) => acc.concat(val), []).join(" ");
-  let flatArr = flat.split(" ");
+  let flat = routes.reduce((acc, val) => acc.concat(val), []);
+  // let flat = routes.reduce((acc, val) => acc.concat(val), []).join(" ");
+  // If array contains only one string
+  if (flat.length === 1) {
+    // filter duplicates
+    let flatSet = new Set(flat[0].split(", "));
+    // convert back to array and return as string
+    return [...flatSet].join(", ");
+  }
+  // return flat
   let map = {};
   let result = [];
   let lastSeen = {};
-  flatArr.forEach((route, index) => {
+  flat.forEach((route, index) => {
     map[index] = route;
     lastSeen[route] = index;
   });
@@ -47,14 +53,22 @@ function findRoutes(routes) {
       }
     }
   }
-  return result.join(" ");
+  return result.join(", ");
 }
-console.log(findRoutes("MNL, TAG, CEB, TAC, TAG, CEB, TAC, BOR"));
+// console.log(findRoutes("MNL, TAG, CEB, TAC, TAG, CEB, TAC, BOR")); //done
+// console.log(findRoutes([["UAE", "NGR"],["JPN", "PHL"],["BRA", "FRA"],["PHL", "BRA"], ["FRA", "ENG"], ["NGR", "JPN"]]));  // done
+// console.log(findRoutes(["USA, BRA, UAE, JPN, PHL, BRA, FRA, PHL, USA"])); // done
+console.log(findRoutes("MNL TAG CEB TAC BOR"));
+// console.log(findRoutes('MNL, TAG, CEB, TAC, TAG, CEB, TAC, BOR'));   // probably done
+
+
+
+
+
+
+// findRoutes(["USA, BRA, UAE, JPN, PHL, BRA, PHL, USA"]);
 
 // findRoutes([["USA", "BRA"], ["JPN", "PHL"], ["BRA", "UAE"], ["UAE", "JPN"]]);
-// findRoutes(["USA, BRA, UAE, JPN, PHL, BRA, PHL, USA"]);
-// console.log(findRoutes('MNL, TAG, CEB, TAC, TAG, CEB, TAC, BOR'));
-// console.log(findRoutes("MNL TAG CEB TAC BOR"));
 
 // function findRoutes(routes) {
 //   let search = "";
@@ -91,3 +105,58 @@ console.log(findRoutes("MNL, TAG, CEB, TAC, TAG, CEB, TAC, BOR"));
 // console.log(
 //   findRoutes([["USA", "BRA"], ["JPN", "PHL"], ["BRA", "UAE"], ["UAE", "JPN"]])
 // );
+
+// function findRoutes(routes) {
+//   //   if (routes.length === 1 && (typeof routes)==='object') {
+//   //     return routes.join(" ")
+//   //   }
+//   if (typeof routes === "string") {
+//     let strArr = routes.split(", ");
+//     // if (strArr.length > 1) {
+//       let strSet = new Set(strArr);
+//       strArr = [...strSet];
+//       return strArr.join(", ");
+//     // } else {
+//     //   strArr = strArr.join(" ");
+//     //   strArr = strArr.split(" ")
+//     //   return strArr.join(", ");
+//     // }
+//   }
+//   let flat = routes.reduce((acc, val) => acc.concat(val), []).join(" ");
+//   return routes
+//   let flatArr = flat.split(" ");
+//   let map = {};
+//   let result = [];
+//   let lastSeen = {};
+//   flatArr.forEach((route, index) => {
+//     map[index] = route;
+//     lastSeen[route] = index;
+//   });
+//   result.push(map[0]);
+//   result.push(map[1]);
+//   let search = map[1];
+//   const len = Object.keys(lastSeen).length;
+//   for (let i = 1; i < len; i++) {
+//     // Even
+//     if (lastSeen[search] && !(lastSeen[search] % 2)) {
+//       result.push(map[lastSeen[search] + 1]);
+//       search = result[result.length - 1];
+//       //  Odd
+//     } else if (lastSeen[search] % 2) {
+//       for (let key in map) {
+//         if (
+//           map[key] == search &&
+//           !(key % 2) &&
+//           map[key * 1 + 1] !== undefined
+//         ) {
+//           result.push(map[key * 1 + 1]);
+//           search = map[key * 1 + 1];
+//           break;
+//         }
+//       }
+//     }
+//   }
+//   return result.join(", ");
+// }
+// // console.log(findRoutes("MNL, TAG, CEB, TAC, TAG, CEB, TAC, BOR")); //done
+// console.log(findRoutes([["UAE", "NGR"],["JPN", "PHL"],["BRA", "FRA"],["PHL", "BRA"], ["FRA", "ENG"], ["NGR", "JPN"]]));  // done
